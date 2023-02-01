9e24cce94360c1df180063927974bbe9b9da45f8
Diana\ Moroz <diva14@yandex.by>
2023-02-01 10:36:58 +0300

    task02 rename function greeting

--- homework_Moroz/homework03/2.js
+++ homework_Moroz/homework03/2.js
@@ -1,6 +1,6 @@
-function helloUser(name) {  
+function greeting(name) {  
          console.log(`Привет, ${name.toUpperCase()}! Мы так рады видеть тебя!`);
     }
     
 const user = prompt('Введите ваше имя');
-helloUser(user)
+greeting(user)
