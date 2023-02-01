95f9eb50d468c483e87b0d7dde8e391d76516d43
Diana\ Moroz <diva14@yandex.by>
2023-02-01 10:35:24 +0300

    task02 with upperCase

--- homework_Moroz/homework03/2.js
+++ homework_Moroz/homework03/2.js
@@ -1,5 +1,5 @@
 function helloUser(name) {  
-         console.log(`Привет, ${name}! Мы так рады видеть тебя`);
+         console.log(`Привет, ${name.toUpperCase()}! Мы так рады видеть тебя!`);
     }
     
 const user = prompt('Введите ваше имя');
