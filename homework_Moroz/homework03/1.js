f310cb85b9242b0fb472ef8f37ad167406b977bb
Diana\ Moroz <diva14@yandex.by>
2023-02-02 10:18:57 +0300

    new const and checked function

--- homework_Moroz/homework03/1.js
+++ homework_Moroz/homework03/1.js
@@ -1,7 +1,8 @@
 const temp = +prompt('Введите темперутуру в цельсиях: ');
-alert(`Цельсий: ${temp} ; Фарегейт ${celsiusToFahrenheit(temp)}`);
+const fahr = celsiusToFahrenheit(temp).toFixed(2)
+alert(`Цельсий: ${temp} ; Фарегейт ${fahr}`);
 
 function celsiusToFahrenheit(sm_temp) {
-    fahrenheit = (sm_temp * 9 / 5) + 32
-    return +fahrenheit.toFixed(2)
+    fahrenheit = (sm_temp * 9 / 5) + 32;
+    return fahrenheit;
 }
